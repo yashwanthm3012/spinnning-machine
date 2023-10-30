55bd5f08454b4c2ce63981dd9a2a0db28d81ac57
quietocean3012 <128483444+cfh3012@users.noreply.github.com>
2023-10-30 22:34:53 +0530

    solve balance problem

--- project.js
+++ project.js
@@ -93,8 +93,80 @@ const spin = () => {
 };
 
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-console.log(reels);
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i< ROWS;i++) {
+        rows.push([]);
+
+        for(let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+
+        for(const [i, symbol] of row.entries()) {
+            rowString += symbol;
+
+            if(i != row.length -1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+        }
+}
+
+
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+    for(let row = 0; row < lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+        // Check each column in the current line
+
+        for(const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
+
+
+const game = () => {
+    let balance = deposit();
+    while (true){
+        console.log("You have a balance of $" + balance)
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance, numberOfLines);
+        balance -= bet * numberOfLines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if(balance <= 0) {
+            console.log("You ran out of money");
+            break;
+        }
+
+        const playAgain = prompt("Do you want to play again (y/n)?");
+
+        if (playAgain != "y") break;
+    }
+};
+
+game();
